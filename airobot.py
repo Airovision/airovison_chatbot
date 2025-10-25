1d34bf7663bea3dc9e6a3f9d9e69824847212ab2
Dasol Oh <dasol@Dasolui-MacBookAir.local>
2025-10-25 20:03:00 +0900

    add: 챗봇 첫 알림 메세지 수정

--- airobot.py
+++ airobot.py
@@ -1,5 +1,6 @@
 import os
 import discord
+from discord import app_commands
 from discord.ui import View, Button
 from dotenv import load_dotenv
 from llava import run_llava, load_llava_model
@@ -9,11 +10,17 @@ load_dotenv()
 discord_key = os.getenv("DISCORD")
 
 intents = discord.Intents.all()
+intents.message_content = True
 client = discord.Client(intents=intents)
 
 CHANNEL_ID = 1427293434796048506
 IMAGE_PATH = "images/sample.jpg"
-ALTER_TEXT = "🚨 건물에 균열이 의심됩니다 🚨\n 이미지를 보고 아래 질문 중 하나를 선택하세요 👇"
+ALTER_TEXT = "⚠️ 손상 감지 ⚠️\n" \
+"새로운 외벽 손상이 탐지되었습니다. 아래의 정보를 확인하세요.\n" \
+"📍 위치    : 인천 미추홀구 인하로 100, 인하대학교용현캠퍼스 하이테크센터\n" \
+"🕒 감지 시각: 2025-10-13 10:24 AM\n" \
+"🏷️ 손상 유형: 콘크리트 균열\n" \
+"🧠 분석 요약: 창문 왼편에 균열이 의심됩니다. 또한 페인트 벗겨짐 등 일부 손상도 확인됩니다. "
 
 
 # 질문 목록 정리
@@ -28,31 +35,35 @@ class QuestionView(View):
     def __init__(self):
         super().__init__(timeout=None)
 
-        # # 버튼 1: 예
-        # self.add_item(Button(label=questions[1], style=discord.ButtonStyle.primary, custom_id="kind"))
-        # # 버튼 2: 아니오
-        # self.add_item(Button(label=questions[2], style=discord.ButtonStyle.primary, custom_id="measure"))
-        # # 버튼 3: 더 설명해줘
-        # self.add_item(Button(label=questions[3], style=discord.ButtonStyle.primary, custom_id="number"))
 
     @discord.ui.button(label=questions[1], style=discord.ButtonStyle.primary)
     async def q1(self, interaction: discord.Interaction, button: Button):
+        await interaction.channel.send(
+        f"{interaction.user.mention}님이 **[{button.label}]** 버튼을 눌렀습니다.\n")
+
         await interaction.response.defer(thinking=True, ephemeral=True)
         result = run_llava(IMAGE_PATH, questions[1])
+
         await interaction.followup.send(result)
 
     @discord.ui.button(label=questions[2], style=discord.ButtonStyle.primary)
     async def q2(self, interaction: discord.Interaction, button: Button):
+        await interaction.channel.send(
+        f"{interaction.user.mention}님이 **[{button.label}]** 버튼을 눌렀습니다.\n")
+
         await interaction.response.defer(thinking=True, ephemeral=True)
         result = run_llava(IMAGE_PATH, questions[2])
+
         await interaction.followup.send(result)
     @discord.ui.button(label=questions[3], style=discord.ButtonStyle.primary)
     async def q3(self, interaction: discord.Interaction, button: Button):
+        await interaction.channel.send(
+        f"{interaction.user.mention}님이 **[{button.label}]** 버튼을 눌렀습니다.\n")
+
         await interaction.response.defer(thinking=True, ephemeral=True)
         result = run_llava(IMAGE_PATH, questions[3])
+
         await interaction.followup.send(result)
-
-
 
 
 @client.event
@@ -62,6 +73,8 @@ async def on_ready():
     if channel is None:
         print("❌ 채널을 찾을 수 없습니다. CHANNEL_ID를 확인하세요.")
         return
+    
+    
 
     load_llava_model()
     # 이미지 파일이 존재하면 전송
@@ -75,11 +88,8 @@ async def on_ready():
 client.run(discord_key)
 
 
-
 # test code
 # @client.event
 # async def on_message(message):
 #     if message.content == "핑":
 #         await message.channel.send("퐁")
-
-client.run(discord_key)
